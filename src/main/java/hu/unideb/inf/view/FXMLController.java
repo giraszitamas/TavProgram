386930a342b52403dce3df4990a799d7a228670f
sanyi <sanyi@GerIsten>
2020-04-13 14:22:40 +0300

    Updated FXML and FXMLController

--- src/main/java/hu/unideb/inf/view/FXMLController.java
+++ src/main/java/hu/unideb/inf/view/FXMLController.java
@@ -12,24 +12,58 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
 
 public class FXMLController implements Initializable {
-    
-    @FXML
-    private Label label;
-    
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        
-        //Save test data
-        Simulation simulation = new Simulation();
-        
-        label.setText("Done!");
-    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    } 
+    
+    @FXML
+    private TextField downSearch;
+
+    @FXML
+    private ListView<?> downOptions;
+    
+    @FXML
+    private Button downLoad;
+
+    @FXML
+    private TextArea downLink;
+    
+    @FXML
+    private Button upLoad;
+
+    @FXML
+    private TextField upSearch;
+
+    @FXML
+    private ListView<?> UpOptions;
+
+    @FXML
+    private TextArea upLink;
+
+    @FXML
+    private TextField upName;
+
+    @FXML
+    void downLoadButtenPushed() {
+         System.out.println("You clicked me!");
+        
+        //Save test data
+        Simulation simulation = new Simulation();
+        
+        System.out.println("Done!");
+
+    }
+
+    @FXML
+    void upLoadButtonPushed() {
+
+    }
 }
