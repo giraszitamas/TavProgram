97812aff64f7fd0b02c30ebb486521620175b035
pixel <pixel@DESKTOP-3CVRL1R>
2020-04-13 13:39:38 +0200

    Code cleared and addet dependency plugin.
    On branch master
    Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   h2-db/test.mv.db
    	modified:   pom.xml
    	modified:
    	src/main/java/hu/unideb/inf/view/FXMLController.java

--- src/main/java/hu/unideb/inf/view/FXMLController.java
+++ src/main/java/hu/unideb/inf/view/FXMLController.java
@@ -1,17 +1,10 @@
 package hu.unideb.inf.view;
 
-import hu.unideb.inf.entity.Course;
-import hu.unideb.inf.entity.User;
 import hu.unideb.inf.modell.Simulation;
-import hu.unideb.inf.util.JpaUserDAO;
-import hu.unideb.inf.util.UserDAO;
 import java.net.URL;
-import java.time.LocalDate;
 import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
