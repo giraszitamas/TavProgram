5e2ac10af18532a57e1dd2b9d0e7ade25cc6771c
sanyi <sanyi@GerIsten>
2020-04-21 18:16:59 +0300

    Added welcome window, redone download and upload window, cleaner FXMLController, added icons

--- src/main/java/hu/unideb/inf/view/FXMLController.java
+++ src/main/java/hu/unideb/inf/view/FXMLController.java
@@ -13,10 +13,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TabPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -26,44 +22,13 @@ public class FXMLController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }
+    
+    //LOGIN START
     @FXML
     private TextField loginUsername;
 
     @FXML
     private PasswordField loginPassword;
-
-    @FXML
-    private TextField downSearch;
-
-    @FXML
-    private ListView<?> downOptions;
-
-    @FXML
-    private TextArea downLink;
-
-    @FXML
-    private Button downLoad;
-    
-    @FXML
-    private Button downClose;
-
-    @FXML
-    private TextField upSearch;
-
-    @FXML
-    private ListView<?> UpOptions;
-
-    @FXML
-    private TextArea upLink;
-
-    @FXML
-    private TextField upName;
-
-    @FXML
-    private Button upLoad;
-    
-    @FXML
-    private Button upClose;
     
     @FXML
     void loginExitButtonPushed() {
@@ -75,15 +40,52 @@ public class FXMLController implements Initializable {
         String flh = loginUsername.getText();
         String jlsz = loginPassword.getText();
         if (flh.equals("megfelelo") && jlsz.equals("ez is")) {
-            windowLoader("/fxml/Scene.fxml", "Felulet");
+            windowLoader("/fxml/Welcome.fxml", "Welcome");
         }
-
+    }
+    //LOGIN END
+   
+    //WELCOME START
+    @FXML
+    void welcomeDownloadButtonPushed() {
+        windowLoader("/fxml/Download.fxml", "Download");
     }
 
     @FXML
-    void downLoadButtenPushed() {
+    void welcomeExitButtonPushed() {
+        System.exit(0);
+    }
+
+    @FXML
+    void welcomeUploadButtonPushed() {
+        windowLoader("/fxml/Upload.fxml", "Upload");
+    }
+    //WELCOME END
+    
+    //DOWNLOAD START
+      @FXML
+    private ListView<?> downloadCourseList;
+
+    @FXML
+    private TextField downloadCourseInput;
+
+    @FXML
+    private ListView<?> downloadNoteList;
+
+    @FXML
+    private TextField downloadCouseInput;
+
+    @FXML
+    private TextArea downloadLink;
+
+    @FXML
+    void downloadBackButtonPushed() {
+        windowLoader("/fxml/Welcome.fxml","Welcome");
+    }
+
+    @FXML
+    void downloadCourseOpenButtonPushed() {
         System.out.println("You clicked me!");
-        
         //Save test data
         Simulation simulation = Simulation.getInstance();
         
@@ -91,26 +93,68 @@ public class FXMLController implements Initializable {
     }
 
     @FXML
-    void exit() {
-        System.exit(0);
-    }
-    @FXML
-    void upExit() {
-        System.exit(0);
-    }
-    @FXML
-    void downExit() {
+    void downloadExitButtonPushed() {
         System.exit(0);
     }
 
     @FXML
-    void upLoadButtonPushed() {
-        
+    void downloadNoteOpenButtonPushed() {
+
     }
+    
+    //DOWNLOAD END
+    
+    //UPLOAD START
+    
+     @FXML
+    private ListView<?> uploadCourseList;
+
+    @FXML
+    private TextField uploadCourseInput;
+
+    @FXML
+    private ListView<?> uploadNoteList;
+
+    @FXML
+    private TextField uploadCouseInput;
+
+    @FXML
+    private TextArea uploadLink;
+
+    @FXML
+    private TextField uploadFileName;
+
+    @FXML
+    void uploadBackButtonPushed() {
+        windowLoader("/fxml/Welcome.fxml","Welcome");
+    }
+
+    @FXML
+    void uploadCourseOpenButtonPushed() {
+
+    }
+
+    @FXML
+    void uploadExitButtonPushed() {
+        System.exit(0);
+    }
+
+    @FXML
+    void uploadNoteOpenButtonPushed() {
+
+    }
+
+    @FXML
+    void uploadUploadButton() {
+
+    }
+    
+    //UPLOAD END
+    
      void windowLoader(String location, String title){
         try {
             Parent parent = FXMLLoader.load(getClass().getResource(location));
-            Stage stage = new Stage(StageStyle.DECORATED);
+            Stage stage = new Stage(StageStyle.UNDECORATED);
             stage.setTitle(title);
             stage.setScene(new Scene(parent));
             stage.show();
@@ -119,5 +163,5 @@ public class FXMLController implements Initializable {
             e.printStackTrace();
         }
     }
-
+     
 }
